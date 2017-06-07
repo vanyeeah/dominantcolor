43e9be0800070738fb7969ee2e36ba5d891776e4
vanyeeah <andreea.mihuta97@gmail.com>
2017-06-07 12:35:19 +0200

    working, less text

--- dominantcolor/build/source/dominantcolor.java
+++ dominantcolor/build/source/dominantcolor.java
@@ -21,7 +21,7 @@ public void setup()
 {
   
   colorMode(HSB, 360, 100, 100);
-  img = loadImage("data/demo.jpg");
+  img = loadImage("data/demo1.jpg");
 }
 
 
@@ -35,7 +35,7 @@ public void draw()
   IntDict pixelBuckets = new IntDict();
 
   // Loop through the images pixels
-  for(int i = 0; i < img.pixels.length;i++) {
+    for(int i = 0; i < img.pixels.length;i++) {
 
     // Get the hue as we are keying the dictionary by the hue
     float pixelHue = hue(img.pixels[i]);
@@ -72,63 +72,16 @@ public void draw()
      float hue = Float.parseFloat(key);
      int theColor = color(Math.round(hue), 100, 75);
 
-      println("Color: " + theColor + " occurs " + i + " times");
+  println("Color: " + theColor + " has been scanned " + i + " times");
 
       fill(theColor);
       rect(20,20,20,20);
 
-     float percentage =  (i * 100.0f) / img.pixels.length;
-     //short percentageAsShort = (short) Math.round(percentage);
 
-     // println("Percentage is:" + percentage + "%");
-     // println("Percentage (short) is:" + percentageAsShort + "%");
-
-     //println(" | hsb: " + int(hue(c)) + "," + int(saturation(c)) + "," + int(brightness(c)));
-
-     short angle = (short)Math.round((360 * (percentage / 100)));
-     angleTotal = angleTotal + angle;
-
-     if (index == pixelBuckets.size() - 1)
-     {
-       // Last peice of pie to be calculated. Ensure ou total sum of angles adds up to 360
-       // to avoid gaps. The reason it does not always round up is likely to be rounding issues
-
-       Integer diff = 360 - angleTotal;
-       pieAngles[index] = (short)(angle + diff);
-     }
-     else
-     {
-       pieAngles[index] = angle;
-     }
-
-     if (angle >= 2)
-     {
-       colors[index] = theColor;
-     }
-     else
-     {
-       // Anything less than a certain angle ignore. Set to -1 and later those colors to be the most common color
-       colors[index] = -1;
-     }
-
-     index++;
-  }
-
-  //println("HashMap Size " + pixelBuckets.size());
-
-  for (int colorIndex = 0; colorIndex < colors.length; colorIndex++)
-  {
-    if (colors[colorIndex] == -1)
-    {
-      colors[colorIndex] = colors[colors.length - 1];
-    }
-  }
 
 
   noLoop();
 }
-
-public void drawPieChart(short[] angles, int[] colors, int diam) {
 }
   public void settings() {  size(400,400); }
   static public void main(String[] passedArgs) {
--- dominantcolor/dominantcolor.pde
+++ dominantcolor/dominantcolor.pde
@@ -5,7 +5,7 @@ void setup()
 {
   size(400,400);
   colorMode(HSB, 360, 100, 100);
-  img = loadImage("data/demo.jpg");
+  img = loadImage("data/demo1.jpg");
 }
 
 
@@ -19,7 +19,7 @@ void draw()
   IntDict pixelBuckets = new IntDict();
 
   // Loop through the images pixels
-  for(int i = 0; i < img.pixels.length;i++) {
+    for(int i = 0; i < img.pixels.length;i++) {
 
     // Get the hue as we are keying the dictionary by the hue
     float pixelHue = hue(img.pixels[i]);
@@ -56,61 +56,14 @@ void draw()
      float hue = Float.parseFloat(key);
      color theColor = color(Math.round(hue), 100, 75);
 
-      println("Color: " + theColor + " occurs " + i + " times");
+  println("Color: " + theColor + " has been scanned " + i + " times");
 
       fill(theColor);
       rect(20,20,20,20);
 
-     float percentage =  (i * 100.0f) / img.pixels.length;
-     //short percentageAsShort = (short) Math.round(percentage);
 
-     // println("Percentage is:" + percentage + "%");
-     // println("Percentage (short) is:" + percentageAsShort + "%");
-
-     //println(" | hsb: " + int(hue(c)) + "," + int(saturation(c)) + "," + int(brightness(c)));
-
-     short angle = (short)Math.round((360 * (percentage / 100)));
-     angleTotal = angleTotal + angle;
-
-     if (index == pixelBuckets.size() - 1)
-     {
-       // Last peice of pie to be calculated. Ensure ou total sum of angles adds up to 360
-       // to avoid gaps. The reason it does not always round up is likely to be rounding issues
-
-       Integer diff = 360 - angleTotal;
-       pieAngles[index] = (short)(angle + diff);
-     }
-     else
-     {
-       pieAngles[index] = angle;
-     }
-
-     if (angle >= 2)
-     {
-       colors[index] = theColor;
-     }
-     else
-     {
-       // Anything less than a certain angle ignore. Set to -1 and later those colors to be the most common color
-       colors[index] = -1;
-     }
-
-     index++;
-  }
-
-  //println("HashMap Size " + pixelBuckets.size());
-
-  for (int colorIndex = 0; colorIndex < colors.length; colorIndex++)
-  {
-    if (colors[colorIndex] == -1)
-    {
-      colors[colorIndex] = colors[colors.length - 1];
-    }
-  }
 
 
   noLoop();
 }
-
-void drawPieChart(short[] angles, color[] colors, int diam) {
 }
